20d7dd957031084943069088a677d6f04530a0ce
VarunIrani <varunirani0505@gmail.com>
2019-10-24 11:07:03 +0530

    Added Search functionality

--- lib/search_tab.dart
+++ lib/search_tab.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'model/app_state_model.dart';
+import 'product_row_item.dart';
+import 'search_bar.dart';
+import 'styles.dart';
 
 class SearchTab extends StatefulWidget {
   @override
@@ -8,14 +13,63 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
+  TextEditingController _controller;
+  FocusNode _focusNode;
+  String _terms = '';
+
+  void _onTextChanged() {
+    setState(() {
+      _terms = _controller.text;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController()..addListener(_onTextChanged);
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildSearchBox() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: SearchBar(
+        controller: _controller,
+        focusNode: _focusNode,
+      ),
+    );
+  } // TO HERE
+
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: const <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Search'),
+    final model = Provider.of<AppStateModel>(context);
+    final results = model.search(_terms);
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Styles.scaffoldBackground),
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            _buildSearchBox(),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) => ProductRowItem(
+                  index: index,
+                  product: results[index],
+                  lastItem: index == results.length - 1,
+                ),
+                itemCount: results.length,
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
