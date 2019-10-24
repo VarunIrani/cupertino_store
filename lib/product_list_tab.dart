0a0d51c761d29dc2df097dafaad774940bd7af34
VarunIrani <varunirani0505@gmail.com>
2019-10-24 10:47:28 +0530

    Displaying Products

--- lib/product_list_tab.dart
+++ lib/product_list_tab.dart
@@ -2,20 +2,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 import 'model/app_state_model.dart';
+import 'product_row_item.dart';
 
 class ProductListTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppStateModel>(
-      builder: (context, model, child) {
-        return CustomScrollView(
-          slivers: const <Widget>[
-            CupertinoSliverNavigationBar(
-              largeTitle: Text('Cupertino Store'),
-            ),
-          ],
-        );
-      },
+    return CupertinoPageScaffold(
+      child: Consumer<AppStateModel>(
+        builder: (context, model, child) {
+          model.loadProducts();
+          final products = model.getProducts();
+          return CustomScrollView(
+            semanticChildCount: products.length,
+            slivers: <Widget>[
+              CupertinoSliverNavigationBar(
+                largeTitle: const Text('Cupertino Store'),
+              ),
+              SliverSafeArea(
+                top: false,
+                minimum: const EdgeInsets.only(top: 8),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      if (index < products.length) {
+                        return ProductRowItem(
+                          index: index,
+                          product: products[index],
+                          lastItem: index == products.length - 1,
+                        );
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
